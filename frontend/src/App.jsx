7c7137f8e6268244c4122445ff8dec56aaaa54ee
Dimalka Navod <dimalkanavod.dnw@gmail.com>
2026-03-04 20:20:20 +0530

    Add routes and create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 
