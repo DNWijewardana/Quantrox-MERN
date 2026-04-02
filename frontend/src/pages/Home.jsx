7c6d69b6ca4224cf9d4b93625c65bebffd5be797
Dimalka Navod <dimalkanavod.dnw@gmail.com>
2026-04-02 19:41:13 +0530

    Fix backend errors and install frontend dependencies

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import HeroSection from '../components/home/HeroSection'
 import FeaturesSection from '../components/home/FeaturesSection'
 import HowItWorksSection from '../components/home/HowItWorksSection'
 import CTASection from '../components/home/CTASection'
+import Header from '../components/layout/Header'
 
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <HeroSection />
       <FeaturesSection />
       <HowItWorksSection />
