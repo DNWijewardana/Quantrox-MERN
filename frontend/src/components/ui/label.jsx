fd91ead3e94a26fce6bbf6de0165946d49514167
Dimalka Navod <dimalkanavod.dnw@gmail.com>
2026-04-21 02:25:52 +0530

    add Upload Plan page with file upload UI and layout

--- frontend/src/components/ui/label.jsx
+++ frontend/src/components/ui/label.jsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { cn } from '../../../lib/utils';
+import { cn } from '@/lib/utils';
 
 const Label = forwardRef(({ className, ...props }, ref) => {
   return (
