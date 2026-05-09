13b5ff5b389ff8a992999c0012f7084488f00df9
Dimalka Navod <dimalkanavod.dnw@gmail.com>
2026-05-10 01:08:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,55 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom"
-import Home from './pages/Home'
-import Login from './pages/auth/Login'
-import Upload from './pages/Upload'
-import Dashboard from './pages/Dashboard'
-import Settings from './pages/Settings'
-import NotFound from './pages/NotFound'
-import Scale from './pages/Scale'
-import Analysis from './pages/Analysis'
-import Editor from './pages/Editor'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import VerifyEmail from "./pages/auth/VerifyEmail";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
+import Upload from "./pages/Upload";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+import Scale from "./pages/Scale";
+import Analysis from "./pages/Analysis";
+import Editor from "./pages/Editor";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
+
       <Routes>
+        {/* Public */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* Authenticated user pages */}
+        <Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="/upload" element={<Upload />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/settings" element={<Settings />} />
         <Route path="/scale" element={<Scale />} />
         <Route path="/analysis" element={<Analysis />} />
         <Route path="/editor" element={<Editor />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings />} />
+
+        {/* Catch-all */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
